70a26393f07d3cf39a682090c050af6e2012efa3
preshu1 <shresthapreshu26@gmail.com>
2026-07-16 15:55:56 +0545

    implement add menu item functionality

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ const restaurant = {
   order: [],
   nextMenuId: 1,
   nextOrderId: 1,
+
+  addMenuItem(name, price, category) {
+    const menuItem = {
+      id: this.nextMenuId,
+      name: name,
+      price: price,
+      category: category,
+      isAvailable: true,
+    };
+    this.menu.push(menuItem);
+    this.nextMenuId++;
+  },
 };
+
+restaurant.addMenuItem("Burger", 450, "main");
+restaurant.addMenuItem("Pizza", 800, "main");
+restaurant.addMenuItem("Coke", 100, "drink");
+restaurant.addMenuItem("Salad", 200, "appetizer");
+console.log(restaurant.menu);
