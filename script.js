98c6e05b3c1e191c75960d43250e2faffda5c8d3
preshu1 <shresthapreshu26@gmail.com>
2026-07-16 16:12:42 +0545

    implement remove menu item functionality

--- script.js
+++ script.js
@@ -16,10 +16,22 @@ const restaurant = {
     this.menu.push(menuItem);
     this.nextMenuId++;
   },
+
+  removeMenuItem(id) {
+    for (let i = 0; i < this.menu.length; i++) {
+      if (this.menu[i].id === id) {
+        const removedMenuItem = this.menu.splice(i, 1);
+        return removedMenuItem[0];
+      }
+    }
+    return null;
+  },
 };
 
 restaurant.addMenuItem("Burger", 450, "main");
 restaurant.addMenuItem("Pizza", 800, "main");
 restaurant.addMenuItem("Coke", 100, "drink");
 restaurant.addMenuItem("Salad", 200, "appetizer");
+console.log(restaurant.menu);
+console.log("Removed menu item: ", restaurant.removeMenuItem(2));
 console.log(restaurant.menu);
